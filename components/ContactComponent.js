b3e84889e458eca8249f91d28f105c8f554619d3
Conor Forrester <conor.forrester214@gmail.com>
2020-01-22 08:16:18 -0500

    Continue Contact Component - add TextInput and TouchableOpacity button

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, ImageBackground, StyleSheet } from 'react-native';
-import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
+import { Text, View, ScrollView, ImageBackground, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
@@ -19,8 +18,37 @@ class Contact extends Component {
                                 <Text style={{color: 'white'}}>Phone: 1-123-456-7890</Text>
                                 <Text style={{color: 'white', marginBottom: 50}}>Email: codecoach@coach.co</Text>
                             </View>
-                            <Text style={{color: 'white', fontSize: 25, left: 20}}>Schedule Your First Session</Text>
+                            <Text style={{color: 'white', fontSize: 20, left: 10}}>Schedule Your First Session</Text>
                             <View style={styles.underlineText}></View>
+                            <View style={styles.form}>
+                                <TextInput 
+                                    style={styles.formText} 
+                                    placeholder="First Name"
+                                >
+
+                                </TextInput>
+                                <TextInput 
+                                    style={styles.formText} 
+                                    placeholder="Last Name"
+                                >
+
+                                </TextInput>
+                                <TextInput 
+                                    style={styles.formText} 
+                                    placeholder="Contact Phone"
+                                >
+
+                                </TextInput>
+                                <TextInput 
+                                    style={styles.formText} 
+                                    placeholder="Contact Email"
+                                >
+
+                                </TextInput>
+                                <TouchableOpacity style={styles.button}>
+                                    <Text style={styles.buttonText}>Schedule</Text>
+                                </TouchableOpacity>
+                            </View>
                         </View>
                     </View>
                 </ScrollView>
@@ -38,9 +66,9 @@ const styles = StyleSheet.create({
         backgroundColor: '#133975',
         opacity: 0.85,
         width: 350,
-        height: 400,
+        height: 600,
         borderRadius: 10,
-        marginTop: 50
+        marginTop: 30
     },
     content: {
         top: 10,
@@ -49,8 +77,32 @@ const styles = StyleSheet.create({
     underlineText: {
         margin: 10,
         borderBottomColor: 'white',
-        borderBottomWidth: 1
+        borderBottomWidth: 0.5
     },
+    form: {
+        marginLeft: 10,
+        marginRight: 100,
+    },
+    formText: {
+        height: 40,
+        color: 'white',
+        marginTop: 12,
+        marginBottom: 10,
+        borderColor: 'white',
+        borderWidth: 1.5,
+        borderRadius: 10,
+        paddingHorizontal: 10   //push the placeholder text in from the left TextInput border
+    },
+    button: {
+        alignItems: 'center',
+        padding: 12,
+        backgroundColor: '#53adc0',
+    },
+    buttonText: {
+        fontSize: 16,
+        color: 'white',
+        fontWeight: 'bold'
+    }
 })
 
 export default Contact;
