dbdf152954d60ff37c279716ce70b63ac9a90c35
Conor Forrester <conor.forrester214@gmail.com>
2020-01-22 19:04:28 -0500

    Finish contact component form - added picker and message box

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, ImageBackground, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, ScrollView, ImageBackground, StyleSheet, Picker, TextInput, TouchableOpacity, Dimensions } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            coach: ''
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    };
+
+    resetForm() {
+        this.setState({
+            coach: ''
+        });
+    }
+    
     render() {
+
         return (
+            <View>
             <ImageBackground source={require('../assets/img/carousel3.jpg')} style={{width: '100%', height: '100%'}}>
-                <ScrollView>
+                <ScrollView contentContainerStyle={{paddingBottom: 20}}>
                     <View style={{flex: 1, alignItems: 'center'}}>
                         <View style={styles.contactContainer}>
                             <View style={styles.content}>
@@ -45,6 +64,28 @@ class Contact extends Component {
                                 >
 
                                 </TextInput>
+                                <Text style={{color: 'white', fontSize: 15, marginTop: 20, marginBottom: 8, fontWeight: 'bold'}}>Select Your Coach</Text>
+                                <View style={styles.pickerView}>
+                                    <Picker
+                                        style={styles.picker}
+                                        selectedValue={this.state.coach}
+                                        onValueChange={itemValue => this.setState({coach: itemValue})}
+                                        >
+                                        <Picker.Item label="Susan Ashcroft" value="1" />
+                                        <Picker.Item label="Mark Wang" value="2" />
+                                        <Picker.Item label="Farah Amin" value="3" />
+                                        <Picker.Item label="Bobby Johnson" value="4" />
+                                    </Picker>
+                                </View>
+                                <Text style={{color: 'white', fontSize: 15, marginTop: 10, marginBottom: 8, fontWeight: 'bold'}}>Your Message</Text>
+                                <View></View>
+                                <TextInput 
+                                    style={styles.message}
+                                    multiline
+                                    numberOfLines={5}
+                                >
+
+                                </TextInput>
                                 <TouchableOpacity style={styles.button}>
                                     <Text style={styles.buttonText}>Schedule</Text>
                                 </TouchableOpacity>
@@ -53,20 +94,17 @@ class Contact extends Component {
                     </View>
                 </ScrollView>
             </ImageBackground>
+            </View>
         );
     }
-
-    static navigationOptions = {
-        title: 'Contact Us'
-    };
 }
 
 const styles = StyleSheet.create({
     contactContainer: {
         backgroundColor: '#133975',
         opacity: 0.85,
-        width: 350,
-        height: 600,
+        width: 270,
+        height: 850,
         borderRadius: 10,
         marginTop: 30
     },
@@ -81,7 +119,7 @@ const styles = StyleSheet.create({
     },
     form: {
         marginLeft: 10,
-        marginRight: 100,
+        marginRight: 10,
     },
     formText: {
         height: 40,
@@ -102,6 +140,28 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: 'white',
         fontWeight: 'bold'
+    },
+    pickerView: {
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 10,
+        marginBottom: 5
+    },
+    picker: {
+        color: 'white',
+        backgroundColor: 'transparent'      //gets rid of the dropdown arrow
+    },
+    message: {
+        height: 40,
+        color: 'white',
+        marginBottom: 50,
+        borderColor: 'white',
+        borderWidth: 1.5,
+        borderRadius: 10,
+        height: 100,
+        textAlignVertical: 'top',
+        paddingHorizontal: 10,
+        paddingVertical: 10
     }
 })
 
